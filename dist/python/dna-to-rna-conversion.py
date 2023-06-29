1824dc129d2f6f155f5d503dd8668de8937523c0
24Ryou <a.yousefzadeh24@gmail.com>
2023-06-30 01:24:52 +0330

    python solution added for dna-to-rna-conversion

---
+++ dist/python/dna-to-rna-conversion.py
@@ -0,0 +1,15 @@
+# 5556282156230d0e5e000089
+# -------------------------------- MY SOLUTION ------------------------------- #
+def dna_to_rna(dna : str):
+    table = str.maketrans("GCAT" , "GCAU")
+    return dna.translate(table)
+# ----------------------------------- TEST ----------------------------------- #
+import codewars_test as test
+
+@test.describe("Sample Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(dna_to_rna("TTTT"), "UUUU")
+        test.assert_equals(dna_to_rna("GCAT"), "GCAU")
+        test.assert_equals(dna_to_rna("GACCGCCGCC"), "GACCGCCGCC")
