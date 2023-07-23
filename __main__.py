6456c5114a6420cfae71f613742115114ebc8010
24Ryou <a.yousefzadeh24@gmail.com>
2023-07-23 21:24:29 +0330

    python solution added for keep-hydrated-1

--- __main__.py
+++ __main__.py
@@ -6,7 +6,8 @@ def main():
     print("What to do?")
     print("0 - Load")
     print("1 - Save")
-    print("2 - Setup")
+    print("2 - No Solutions")
+    print("3 - Setup")
 
     _ = input(("Enter number: "))
 
@@ -14,8 +15,11 @@ def main():
         load()
     elif _ == "1":
         save()
+    elif _ == "2":
+        get_none_solutions()
     else:
         setup()
+        
 
 
 if __name__ == "__main__":
