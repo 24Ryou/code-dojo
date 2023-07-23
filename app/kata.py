a89af2db393229a6a7239c346debc0ce6c7448fc
24Ryou <a.yousefzadeh24@gmail.com>
2023-07-23 21:25:27 +0330

    python solution added for alternating-case-<-equals->-alternating-case

--- app/kata.py
+++ app/kata.py
@@ -1,17 +1,42 @@
-# https://www.codewars.com/kata/582cb0224e56e068d800003c
+# https://www.codewars.com/kata/56efc695740d30f963000557
 # --------------------------------- SOLUTION --------------------------------- #
-def litres(time):
-    return int(time * 0.5)
+def to_alternating_case(string : str):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    translation_table = str.maketrans(letters + letters.upper(), letters.upper() + letters)
+    return string.translate(translation_table)
 # ----------------------------------- TEST ----------------------------------- #
 import codewars_test as test
-@test.describe('Fixed tests')
-def basic_tests():
-    @test.it('Basic Test Cases')
-    def basic_test_cases():
-        test.assert_equals(litres(2), 1, 'should return 1 litre')
-        test.assert_equals(litres(1.4), 0, 'should return 0 litres')
-        test.assert_equals(litres(12.3), 6, 'should return 6 litres')
-        test.assert_equals(litres(0.82), 0, 'should return 0 litres')
-        test.assert_equals(litres(11.8), 5, 'should return 5 litres')
-        test.assert_equals(litres(1787), 893, 'should return 893 litres')
-        test.assert_equals(litres(0), 0, 'should return 0 litres')
+@test.describe("Basic tests")
+def test_bacics():
+    @test.it("should work for fixed tests (provided in the description)")
+    def test_fixed():
+        test.assert_equals(to_alternating_case("hello world"), "HELLO WORLD")
+        test.assert_equals(to_alternating_case("HELLO WORLD"), "hello world")
+        test.assert_equals(to_alternating_case("hello WORLD"), "HELLO world")
+        test.assert_equals(to_alternating_case("HeLLo WoRLD"), "hEllO wOrld")
+        test.assert_equals(to_alternating_case("12345"), "12345")
+        test.assert_equals(to_alternating_case("1a2b3c4d5e"), "1A2B3C4D5E")
+        test.assert_equals(to_alternating_case("String.prototype.toAlternatingCase"), "sTRING.PROTOTYPE.TOaLTERNATINGcASE")
+        test.assert_equals(to_alternating_case(to_alternating_case("Hello World")), "Hello World")
+        
+        
+    @test.it("should work for the title of this Kata")
+    def test_title():
+        title = "altERnaTIng cAsE"
+        title = to_alternating_case(title)
+        test.assert_equals(title, "ALTerNAtiNG CaSe")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "altERnaTIng cAsE")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "ALTerNAtiNG CaSe")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "altERnaTIng cAsE")
+        title = "altERnaTIng cAsE <=> ALTerNAtiNG CaSe"
+        title = to_alternating_case(title)
+        test.assert_equals(title, "ALTerNAtiNG CaSe <=> altERnaTIng cAsE")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "altERnaTIng cAsE <=> ALTerNAtiNG CaSe")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "ALTerNAtiNG CaSe <=> altERnaTIng cAsE")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "altERnaTIng cAsE <=> ALTerNAtiNG CaSe")
